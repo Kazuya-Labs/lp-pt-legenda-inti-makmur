242900d45add2c247967c4a8c8b84d5dca18ee52
Kazuya-Labs <zuyabizon01@gmail.com>
2026-05-16 07:16:19 +0700

    menghapus file mp4 mengubah inframe

---
+++ src/config/galery_produk.js
@@ -0,0 +1,20 @@
+const produk = [
+  {
+    link: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Freel%2F938154385666296%2F&show_text=false&width=269&t=0",
+    desk: "Proses Pembuatan Sapu Plastik",
+  },
+  {
+    link: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Freel%2F1711131103391843%2F&show_text=false&width=269&t=0",
+    desk: "Proses Pembuatan Meja Komputer",
+  },
+  {
+    link: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Freel%2F1233316945519660%2F&show_text=false&width=264&t=0",
+    desk: "Proses Packing Gagang Pel",
+  },
+  {
+    link: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Freel%2F2036233203606661%2F&show_text=false&width=267&t=0",
+    desk: "Otw Pengiriman",
+  },
+];
+
+export default produk

20d2d4dc217dbbb7e06097c7a4493d0f33dadf9b
Kazuya-Labs <zuyabizon01@gmail.com>
2026-05-18 14:13:01 +0700

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://legendaintimakmur.com',
   vite: {
     plugins: [tailwindcss()]
   }

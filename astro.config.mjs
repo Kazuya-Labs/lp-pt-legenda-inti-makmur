a43ecec24444991ce766aedbfefb8f76aae6b97d
Kazuya-Labs <zuyabizon01@gmail.com>
2026-06-14 13:11:41 +0700

    hh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://legendaintimakmur.com',
+  site: "http://localhost:4321",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
+
